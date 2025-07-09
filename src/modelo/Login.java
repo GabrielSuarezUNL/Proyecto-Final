81eb5b0f98dac917c3ccbc1fcc90c7c210babc7b
Jorge <cokimanuel16@gmail.com>
2025-07-09 13:04:54 -0500

    Jorge

---
+++ src/modelo/Login.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author villa
+ */
+public class Login {
+    private String nombre;
+    private String clave ;
+
+    public Login(String nombre, String clave) {
+        this.nombre = nombre;
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+    
+    
+    
+}
